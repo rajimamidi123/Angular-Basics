c5614eb5e6d9bbb1460651127caa12941fcb2d93
Rajeshwari <rajimamidi803@gmail.com>
2022-12-05 20:45:31 +0530

    get data

--- dmantzproject/src/app/app.component.ts
+++ dmantzproject/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dmantzproject';
+  title = 'dmantzproject'
+  data:string='';
+  get(event:any){
+    this.data=event.target.value;
+  }
 }
